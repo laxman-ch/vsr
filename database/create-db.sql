0083c0f7829a6f60e435b5db4acdae25c15ef135
laxman <laxman@ubuntu>
2015-10-15 00:48:24 +0530

    updated database model

--- database/create-db.sql
+++ database/create-db.sql
@@ -1,74 +1,120 @@
-CREATE DATABASE `vsr` /*!40100 DEFAULT CHARACTER SET latin1 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+DROP SCHEMA IF EXISTS `vsr` ;
+CREATE SCHEMA IF NOT EXISTS `vsr` DEFAULT CHARACTER SET latin1 ;
+USE `vsr` ;
+
+-- -----------------------------------------------------
+-- Table `vsr`.`department`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vsr`.`department` (
+  `dept_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `address` VARCHAR(256) NOT NULL,
+  `details` VARCHAR(256) NULL DEFAULT NULL,
+  `dept_name` VARCHAR(64) NOT NULL,
+  `dept_code` VARCHAR(16) NOT NULL,
+  PRIMARY KEY (`dept_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
+
+CREATE UNIQUE INDEX `uk_dept_code` ON `vsr`.`department` (`dept_code` ASC);
+
+CREATE UNIQUE INDEX `uk_dept_name` ON `vsr`.`department` (`dept_name` ASC);
 
 
-CREATE TABLE `station` (
-  `id` int(11) NOT NULL,
-  `short_code` varchar(6) NOT NULL COMMENT 'Easy to remember short code for station.\nFor example: HYD for HYDERABAD',
-  `station_name` varchar(32) NOT NULL COMMENT 'Name of the station',
-  `district` varchar(32) NOT NULL,
-  `state` varchar(32) NOT NULL,
+-- -----------------------------------------------------
+-- Table `vsr`.`station`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vsr`.`station` (
+  `stn_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `district` VARCHAR(32) NOT NULL,
+  `stn_code` VARCHAR(6) NOT NULL,
+  `state` VARCHAR(32) NOT NULL,
+  `stn_name` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`stn_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = latin1;
+
+CREATE UNIQUE INDEX `uk_stn_code` ON `vsr`.`station` (`stn_code` ASC);
+
+CREATE UNIQUE INDEX `uk_stn_name` ON `vsr`.`station` (`stn_name` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `vsr`.`faremap`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vsr`.`faremap` (
+  `fare_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `fare` FLOAT NOT NULL,
+  `from_stn_id` INT(11) NOT NULL,
+  `to_stn_id` INT(11) NOT NULL,
+  PRIMARY KEY (`fare_id`),
+  CONSTRAINT `fk_fares_tostn`
+    FOREIGN KEY (`to_stn_id`)
+    REFERENCES `vsr`.`station` (`stn_id`),
+  CONSTRAINT `fk_fares_fromstn`
+    FOREIGN KEY (`from_stn_id`)
+    REFERENCES `vsr`.`station` (`stn_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
+
+CREATE UNIQUE INDEX `uk_fares_fromto` ON `vsr`.`faremap` (`from_stn_id` ASC, `to_stn_id` ASC);
+
+CREATE INDEX `fk_fares_tostn` ON `vsr`.`faremap` (`to_stn_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `vsr`.`invoice`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vsr`.`invoice` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `article_charges` FLOAT NOT NULL,
+  `date` DATETIME NOT NULL,
+  `dc_number` VARCHAR(32) NOT NULL,
+  `description` VARCHAR(256) NULL DEFAULT NULL,
+  `doordel_charges` FLOAT NOT NULL,
+  `freight` FLOAT NOT NULL,
+  `from_address` VARCHAR(256) NULL DEFAULT NULL,
+  `goods_value` FLOAT NOT NULL,
+  `handling_charges` FLOAT NOT NULL,
+  `other_charges` FLOAT NOT NULL,
+  `package_count` INT(11) NOT NULL,
+  `stat_charges` FLOAT NOT NULL,
+  `to_address` VARCHAR(256) NULL DEFAULT NULL,
+  `total` FLOAT NOT NULL,
+  `value_surcharge` FLOAT NOT NULL,
+  `waybill_number` VARCHAR(16) NOT NULL,
+  `weight` FLOAT NOT NULL,
+  `dept_id` INT(11) NOT NULL,
+  `from_stn_id` INT(11) NOT NULL,
+  `to_stn_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_short_code` (`short_code`),
-  UNIQUE KEY `station_name_UNIQUE` (`station_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_invc_tostn`
+    FOREIGN KEY (`to_stn_id`)
+    REFERENCES `vsr`.`station` (`stn_id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_invc_deptid`
+    FOREIGN KEY (`dept_id`)
+    REFERENCES `vsr`.`department` (`dept_id`),
+  CONSTRAINT `fk_invc_fromstn`
+    FOREIGN KEY (`from_stn_id`)
+    REFERENCES `vsr`.`station` (`stn_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE `department` (
-  `id` int(11) NOT NULL,
-  `short_code` varchar(16) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `address` varchar(256) NOT NULL,
-  `details` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE INDEX `fk_invc_deptid` ON `vsr`.`invoice` (`dept_id` ASC);
 
-CREATE TABLE `faremap` (
-  `from_station_id` int(11) NOT NULL,
-  `to_station_id` int(11) NOT NULL,
-  `fare` float NOT NULL,
-  PRIMARY KEY (`from_station_id`,`to_station_id`),
-  KEY `fk_faremap_toid_idx` (`to_station_id`),
-  CONSTRAINT `fk_faremap_fromid` FOREIGN KEY (`from_station_id`) REFERENCES `station` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_faremap_toid` FOREIGN KEY (`to_station_id`) REFERENCES `station` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE INDEX `fk_invc_fromstn` ON `vsr`.`invoice` (`from_stn_id` ASC);
 
-CREATE TABLE `invoice` (
-  `id` bigint(20) NOT NULL,
-  `waybill_number` varchar(16) NOT NULL,
-  `dept_id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `from_station_id` int(11) NOT NULL,
-  `to_station_id` int(11) NOT NULL,
-  `dc_number` varchar(32) NOT NULL,
-  `from_address` varchar(256) DEFAULT NULL,
-  `to_address` varchar(256) DEFAULT NULL,
-  `goods_value` float NOT NULL,
-  `package_count` int(11) NOT NULL,
-  `description` varchar(256) DEFAULT NULL,
-  `weight` float NOT NULL,
-  `freight` float NOT NULL,
-  `value_surcharge` float NOT NULL,
-  `doordel_charges` float NOT NULL,
-  `article_charges` float NOT NULL,
-  `stat_charges` float NOT NULL,
-  `handling_charges` float NOT NULL,
-  `other_charges` float NOT NULL,
-  `total` float NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_from_stn_id_idx` (`to_station_id`,`from_station_id`),
-  KEY `fk_dept_id_idx` (`dept_id`),
-  CONSTRAINT `fk_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `department` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_from_stn_id` FOREIGN KEY (`to_station_id`, `from_station_id`) REFERENCES `faremap` (`to_station_id`, `from_station_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE INDEX `fk_invc_tostn` ON `vsr`.`invoice` (`to_stn_id` ASC);
 
-CREATE TABLE `task_list` (
-  `task_id` int(11) NOT NULL AUTO_INCREMENT,
-  `task_name` varchar(100) NOT NULL,
-  `task_description` varchar(255) DEFAULT NULL,
-  `task_priority` varchar(20) DEFAULT NULL,
-  `task_status` varchar(20) DEFAULT NULL,
-  `task_start_time` datetime DEFAULT NULL,
-  `task_end_time` datetime DEFAULT NULL,
-  `task_archived` int(11) DEFAULT '0',
-  PRIMARY KEY (`task_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
